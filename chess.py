22097de8e3d3cf5d9a60863608000c745d70dbf7
Willie Chalmers III <willieechalmers@gmail.com>
2017-11-24 22:39:18 -0600

    Fix chess.py

--- chess.py
+++ chess.py
@@ -1,11 +1,12 @@
-Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)] on win32
-Type "copyright", "credits" or "license()" for more information.
->>> if _name_ is '_main_':
-	print ('Hello World')
+#!/usr/bin/env python
+"""
+A game of chess that uses quantum rules, has pieces that move diagonally, and involves four players
+"""
 
-	
-Traceback (most recent call last):
-  File "<pyshell#2>", line 1, in <module>
-    if _name_ is '_main_':
-NameError: name '_name_' is not defined
->>> 
+
+def main():
+    print('Hello world!')
+
+
+if __name__ == '__main__':
+    main()
