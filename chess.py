aa0cb52336182e849e53c47e99848132a39d6e2b
Raquel Whitmire <rockevelation@gmail.com>
2017-11-24 19:55:03 -0800

    chess

---
+++ chess.py
@@ -0,0 +1,11 @@
+Python 3.6.3 (v3.6.3:2c5fed8, Oct  3 2017, 18:11:49) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> if _name_ is '_main_':
+	print ('Hello World')
+
+	
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    if _name_ is '_main_':
+NameError: name '_name_' is not defined
+>>> 
